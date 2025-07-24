4e72d87aa7e8058f8992daca9f360bcff3ec5622
kumar.shashank <kumar.shashank@instructure.com>
2025-07-24 23:28:31 +0530

    Handle valid positive numbers input string scenario in String calculator

--- app/domain/services/string_calculator.rb
+++ app/domain/services/string_calculator.rb
@@ -5,10 +5,17 @@ module Services
 
     def initialize(input_string)
       @input_string = input_string
+      @sanitized_array = sanitize
     end
 
     def add
       return 0 if @input_string.empty?
+
+      @sanitized_array.map(&:to_i).sum
+    end
+
+    def sanitize
+      @input_string.delete('^0-9-').split('')
     end
   end
 end
